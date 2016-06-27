a7dace51aeb99b0fc579ee1f5548d606803750e5
Mark Brown II <mbrownii@doc.gov>
2016-06-27 18:21:52 -0400

    Incomplete commit

--- scripts/csv_json.py
+++ scripts/csv_json.py
@@ -2,13 +2,13 @@ import csv, ujson as json, pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-def mapjson(course, uid, course_key = ['original_date','course_name','recommended_prerequisite','instructor','in_person_ta','webex_ta','course_length','description','topics_covered', 'additional_resources', 'register', 'software_needed']):
+def mapjson(course, uid, course_key = ['original_date','course_name','recommended_prerequisite','instructor','in_person_ta','webex_ta','course_length','description','topics_covered', 'additional_resources', 'register', 'software_needed', 'class_type']):
     course_entry = { key: value for (key, value) in zip(course_key, course) }
     course_entry['uid'] = uid
     return course_entry
 
 course_json = []
-with open('./Commerce Data Academy Courses - Academy_Courses_Reduced.csv', 'rt') as f:
+with open('./Commerce Data Academy Courses - MarksVersion.csv', 'rt') as f:
     courses = csv.reader(f)
     for i, course in enumerate(courses):
         # if i == 0:
