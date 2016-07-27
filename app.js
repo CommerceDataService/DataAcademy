29fae98d9c3cbaa9b7df7ee3364edcbb0ffe059a
Mark Brown II <mbrownii@doc.gov>
2016-07-27 14:58:04 -0400

    Add bodyParser and path to app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
 
 app.use(express.static(__dirname + "/"));
 
 app.get('/detailee', function(req,res) {
 	res.sendFile(__dirname + "/detailee_info.html")
-})
+});
 
 app.listen(3000);
