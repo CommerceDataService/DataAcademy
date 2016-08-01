5db5f5b2292963f9cca59c23567b2ceef6eecfd1
Mark Brown II <mbrownii@doc.gov>
2016-08-01 12:16:07 -0400

    Setting up node config

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var hbs = require('express-hbs');
 
 
-app.use(express.static(__dirname + "/"));
+// app.use(express.static(__dirname + "/public/index.html"));
+app.engine('hnbs', hbs.express4({
+	defaultLayout: 'main',
+	partialsDir: path.join(__dirname, "views/partials")
+})); // for the 'hnbs' file extension, use the hbs.require
+
+app.set('view engine', 'hnbs');  // use 'hnbs' file extension when omitted from file
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/detailee', function(req,res) {
-	res.sendFile(__dirname + "/detailee_info.html")
+	res.sendFile(path.join(__dirname, "public/detailee_info.html"));
 });
 
-app.listen(3000);
+app.get('/courses', function(req, res) {
+	res.sendFile(__dirname + "/public/courses.html")
+});
+
+console.log(__dirname);
+
+app.listen(app.get('port'));
---
+++ things_I_learn.md
@@ -0,0 +1,13 @@
+1. Express tutorial: https://www.packtpub.com/books/content/using-handlebars-express
+
+2. App.set vs. app.listen - http://stackoverflow.com/questions/25337222/javascript-app-setport-8080-versus-app-listen8080-in-express-js
+
+http://stackoverflow.com/questions/18864677/what-is-process-env-port-in-node-js -- so to not hard code port number
+
+3. path.join - https://nodejs.org/api/path.html#path_path_join_path
+
+4. template engines: https://expressjs.com/en/guide/using-template-engines.html
+
+5. view engine setting: https://expressjs.com/en/4x/api.html#app.set -- determines the file extension
+
+6. app.engine - http://expressjs.com/en/api.html#app.engine
