ebbebade45a1052a48796c96ffca51eab9bcad21
Mark Brown II <mbrownii@doc.gov>
2016-08-04 14:14:52 -0400

    Change view directory to be on parent level

--- app.js
+++ app.js
@@ -7,21 +7,16 @@ var fs = require('fs');
 var jsonQuery = require('json-query');
 
 
-// var options = {
-// 	extensions: ['html'],
-// 	index: false
-// };
 
 
 
 
-// app.use(express.static(__dirname + "/public/index.html"));
 app.engine('handlebars', exphbs({
-	defaultLayout: 'main',
-	layoutsDir: __dirname + "/public/views/layouts/"
+	defaultLayout: 'main'
+	// layoutsDir: __dirname + "/views/layouts/"
 }));
 
-app.set('views', __dirname + '/public/views');
+// app.set('views', __dirname + '/public/views');
 
 app.set('view engine', 'handlebars');
 
@@ -38,35 +33,17 @@ app.get('/', function(req,res) {
 	res.render('titlePage');
 });
 
-// app.get('/detailee/:name', function(req,res) {
-// 	// res.sendFile(path.join(__dirname, "public/detailee_info.html"));
-// 	var clickedName = req.params.name;
-// });
-
-// app.get('/detailee', function(req,res) {
-// 	// res.send('Whee');
-// 	// var val = req.query.name;
-// 	console.log(req.query);
-// 	res.send('whee');
-// });
-
 app.get('/detailee', function(req,res) {
 	res.render('detailee');
 })
 
 app.get('/detailee/:name', function(req, res) {
 	var val = req.params.name;
-	console.log(val);
 	var text = fs.readFileSync('./scripts/detailee_info.json', 'utf8')
-	console.log(text);
-	console.log(typeof text);
 	var obj = JSON.parse(text);
 	var queryName = jsonQuery('data[0].' + val + '.name', {data: obj}).value;
 	var queryPic = jsonQuery('data[0].' + val + '.pic', {data: obj}).value;
 	var queryBlog =jsonQuery('data[0].' + val + '.blog', {data: obj}).value
-	console.log(obj);
-	console.log('queryName = ' + queryName);
-	console.log(typeof obj);
 	res.render('detailee_info', {
 		queryName: queryName,
 		queryPic: queryPic,
