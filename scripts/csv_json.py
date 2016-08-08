def7bdb6e6e5f71fe98292a2955fddce08390159
Mark Brown II <mbrownii@doc.gov>
2016-08-08 18:07:08 -0400

    Add more fields to python script for CSV->JSON
    Added in more fields that will be used in Handlebars template
    Added 'a' tags to courses layout for courses

--- scripts/csv_json.py
+++ scripts/csv_json.py
@@ -2,7 +2,7 @@ import csv, ujson as json, pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-def mapjson(course, uid, course_key = ['original_date','course_name','recommended_prerequisite','instructor','in_person_ta','webex_ta','course_length','description','topics_covered', 'additional_resources', 'register', 'software_needed', 'class_type']):
+def mapjson(course, uid, course_key = ['original_date','course_name','prereq_1_name','prereq_1_link','prereq_2_name','prereq_2_link','instructor','course_length','description','topics_covered', 'additional_resources', 'register', 'software_needed', 'class_type','website_identifier','slidedeck','video_download','additional_materials','related_class_1_name','related_class_1_link','related_class_2_name','related_class_2_link','related_class_3_name','related_class_3_link','level','youtube_link']):
     course_entry = { key: value for (key, value) in zip(course_key, course) }
     course_entry['uid'] = uid
     return course_entry
